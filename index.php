8e0e161063e5ed8897b397de338f35f78cab26f8
Ivaykin Timofey <ivaykin@live.com>
2014-12-26 07:55:40 +1000

    Изменения внешнего вида

--- index.php
+++ index.php
@@ -25,24 +25,33 @@
 
 <body>
 	<div id="top">
-		<b><a href="/">
-			<div class="logo"></div>
-		</a></b>
-		
-		<div class="phone">
-			<b>8-800-555-36-75</b>
-			<a href="http://www.skatpower.ru/">http://wwww.skatpower.ru</a>
-		</div>
-		<br>
+		<div class="top-main">
+			<div style="height: 120px;">
+				<a href="/">
+					<div class="logo"></div>
+				</a>
+				
+				<div class="phone">
+					<b>8-800-555-36-75</b>
+					<a href="http://www.skatpower.ru/">http://wwww.skatpower.ru/</a>
+				</div>
+			</div>
+			<div class="centered" style="margin-top: 51px;">
+				<div class="title-text"><b>Надежные и практичные</b><br>генераторы Skat 6 квт</div>
+				
+				<div style="height: 100px; margin-top: -90px;">
+					<a href="http://www.skatpower.ru/shops.html">
+						<div class="button-dill"></div>
+					</a>
+				</div>
 
-		<div class="button-dill"></div>
-		<div class="title-text"><b>Надежные и практичные</b><br>генераторы Skat 6 квт</div>
+			</div>
+			<div class="gen" ></div>
+			<div class="button-top">
+				
 
-		<div class="gen" ></div>
-		<div class="button-top">
-			<br>
-
-			<b>посмотреть линейку 6 кВт генераторов</b>
+				<a href="#compare"><b>ПОСМОТРЕТЬ ЛИНЕЙКУ 6 кВт ГЕНЕРАТОРОВ</b></a>
+			</div>
 		</div>
 	</div>
 	<!---->
@@ -282,60 +291,6 @@
 			</div>
 			<div class="comparison-table"></div>
 		</div>
-		
-		<!--<table>
-			<tr class="title-table">
-				<th class="main-title-table">сравнение <br> моделей</th>
-				<th>Стандартная <br> модель<br><b>Угб-6000</b></th>
-				<th>С электро <br> запуском<br><b>Угб-6000Е</b></th>
-				<th>С авто <br> запуском<br><b>Угб-6000Авто</b></th>
-				<th>облегченная <br> версия<br><b>Угб-6000basic</b></th>
-			</tr>
-			<tr>
-				<th>тип запуска</th>
-				<td>Ручной</td>
-				<td><b>Ручной/ <br> Электро</b></td>
-				<td><b>Ручной/ <br> Электро/Авто</b></td>
-				<td>Ручной</td>
-								
-			<tr>
-				<th>Вес, кг</th>
-				<td class="bg-table">79,2</td>
-				<td class="bg-table">86,7</td>
-				<td class="bg-table">87,4</td>
-				<td class="bg-table"><b>69,8</b></td>
-			</tr>
-			</tr>
-			<tr>
-				<th>Габариты, мм</th>
-				<td colspan="3" class="spacer-table1">715x570x600</td>
-				<td><b>700x540x600</b></td>
-			</tr>
-			<tr>
-				<th>работа на одном баке, Ч</th>
-				<td colspan="3" class="spacer-table2"><b>10</b></td>
-				<td class="bg-table">2,7</td>
-			</tr>
-			<tr>
-				<th>Температура запуска, °С</th>
-				<td colspan="4" class="spacer-table3">-15°с</td>
-			</tr>
-			<tr>
-				<th>Расход топлива, Л/Ч</th>
-				<td colspan="4" class="spacer-table3-bg">2,5</td>
-			</tr>
-			<tr>
-				<th>Уровень шума, дБ</th>
-				<td colspan="4" class="spacer-table3">76</td>
-			</tr>
-			<tr>
-				<th>Количество розеток</th>
-				<td colspan="2" class="bg-table">16A – 2 шт <br><b>32А – 1 шт</b></td>
-				<td class="bg-table">16A – 1 шт <br><b>32А – 1 шт</b></td>
-				<td class="bg-table">16A – 2 шт</td>
-			</tr>
-		</table>
-	-->
 	<div class="centered inventor-info">
 		<div class="ico"></div>
 		К этим генераторам можно подключать сварочные инвенторы с силой сварочного тока до 160А
@@ -348,33 +303,146 @@
 	</div>
 	<!---->
 	<div id="advantage">
-		
+		<div class="advantage-img"></div>
 	</div>
 	<!---->
 	<div id="protect">
-		
+		<div class="protect-img"></div>
 	</div>
 	<!---->
 	<div id="causes">
-		<h1><br><b>7 причин, почему любой из генераторов СКАТ<b> <br>прослужит вам долго</h1>
+		<!--<h1><br><b>7 причин, почему любой из генераторов СКАТ<b> <br>прослужит вам долго</h1>
 		<h3>Генератор СКАТ надежно защищен от:</h3>
 		<div class="image-level1">
 			<p><br><br><br>Перегрузок&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Попадания ржавчины в бак&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Запуска без масла</p>
 		</div>
 		<div class="image-level2">
 			<p><br><br><br>Пыли и грязи&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Брызг&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Сгорания обмотки&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Перегрева</p>
-		</div>
+		</div>-->
+		<br><br><br><br>
+		<div class="causes-all"></div>
 	</div>
 	<!---->
 	<div id="compare2">
-		
+		<div class="generators-cards-with-comparison phase-one gas">
+			<h1><b>1-фазные бензиновые</b> 6 кВт генераторы</h1>
+			
+			<table class="generators-cards-centered-block">
+				<tr>
+					<td>
+						<div class="generator-card УГБ-6000">
+							<header>УГБ-6000</header>
+							<article>
+								<table>
+									<tr>
+										<td>
+											<div class="generator-image"></div>
+										</td>
+										<td>
+											<div class="generator-description">Бензиновый генератор 6 кВт с ручным пуском</div>
+										</td>
+									</tr>
+								</table>
+							</article>
+						</div>
+					</td>
+					<td class="middle-space"></td>
+					<td>
+						<div class="generator-card УГБ-6000E">
+							<header>УГБ-6000E</header>
+							<article>
+								<table>
+									<tr>
+										<td>
+											<div class="generator-image"></div>
+										</td>
+										<td>
+											<div class="generator-description">Бензиновый генератор 6 кВт с электропуском</div>
+										</td>
+									</tr>
+								</table>
+							</article>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="generator-card УГБ-6000Авто">
+							<header>УГБ-6000Авто</header>
+							<article>
+								<table>
+									<tr>
+										<td>
+											<div class="generator-image"></div>
+										</td>
+										<td>
+											<div class="generator-description">Бензиновый генератор 6 кВт с автозапуском</div>
+										</td>
+									</tr>
+								</table>
+							</article>
+						</div>
+					</td>
+					<td class="middle-space"></td>
+					<td>
+						<div class="generator-card УГБ-6000Basic">
+							<header>УГБ-6000Basic</header>
+							<article>
+								<table>
+									<tr>
+										<td>
+											<div class="generator-image"></div>
+										</td>
+										<td>
+											<div class="generator-description">Бензиновый генератор 6 кВт мобильный и компактный</div>
+										</td>
+									</tr>
+								</table>
+							</article>
+						</div>
+					</td>
+				</tr>
+			</table>
+		</div>
 	</div>
 	<!---->
 	<div id="company">
-		
+		<div class="company-img"></div>
 	</div>
 	<!---->
 	<div id="service">
+		
+		<div class="service-img"></div>
+		<!--<table class="centered">
+			<tr>
+				<td>
+					<div class="parameter-selector phase-selector">
+						<header>
+							<strong><h2>Генератор СКАТ прост <br>в эксплуатации и обслуживании</h2></strong>
+						</header>
+						<article class="phase-one">
+							<p>Благодаря механическому счетчику моточасов,<br>вы не пропустите момент, когда нужно произвести<br>техобслуживание генератора: сменить масло, поменять<br>свечу, почистить фильтры. Это обеспечит долгую жизнь<br> вашему генератору.</p>	
+						</article>
+						
+					</div>
+				</td>
+				<td class="middle-space"></td>
+				<td>
+					<div class="parameter-selector fuel-selector">
+						<header>
+							<strong><h2>Информационная <br>и техническая поддержка</h2></strong>
+						</header>
+						<article class="gas">
+						<p>Вы всегда сможете получить техническую и <br>информационную поддержку о техобслуживании <br>генератора, схемах подключения и любых других <br>вопросах, которые могут у вас возникнуть.</p>	
+						</article>
+
+					</div>
+				</td>
+			</tr>
+			
+			
+		</table>
+		
 		<div class="easy-service">
 			<h2>Генератор СКАТ прост <br>в эксплуатации и обслуживании</h2>
 			<p>Благодаря механическому счетчику моточасов,<br>вы не пропустите момент, когда нужно произвести<br>техобслуживание генератора: сменить масло, поменять<br>свечу, почистить фильтры. Это обеспечит долгую жизнь<br> вашему генератору.</p>	
@@ -382,7 +450,7 @@
 		<div class="info-service">
 			<h2>Информационная <br>и техническая поддержка</h2>
 			<p>Вы всегда сможете получить техническую и <br>информационную поддержку о техобслуживании <br>генератора, схемах подключения и любых других <br>вопросах, которые могут у вас возникнуть.</p>	
-		</div>
+		</div>-->
 	</div>
 	<!---->
 	<div id="garanty">
@@ -393,7 +461,7 @@
 	</div>
 	<!---->
 	<div id="location">
-	<div class="map">
+	<!--<div class="map">
 		<div class="fleft">
 			<form action="">
 				<select id="cd-dropdown" class="cd-select" onchange="alert(tdis.options[tdis.selectedIndex].value);">
@@ -414,22 +482,37 @@
 
 			});
 
-		</script>
-
+		</script>-->
+	<div class="map-space"></div>
+	<a href="http://www.skatpower.ru/shops.html">
+		<div class="button-dill"></div>
+	</a>
 	</div>
 	<!---->
 	<div id="footer">
-		<b><a href="/">
-			<div class="logo"></div>
-		</a></b>
-		
-		<div class="phone">
-			<b>8-800-555-36-75</b>
-			<a href="http://www.skatpower.ru/">http://wwww.skatpower.ru</a>
+		<div class="footer-fix">
+			<b><a href="/">
+				<div class="logo"></div>
+			</a></b>
+			
+			<div class="phone">
+				<b>8-800-555-36-75</b>
+				<a href="http://www.skatpower.ru/">http://wwww.skatpower.ru/</a>
+			</div>
+			<br>
 		</div>
-		<br>
 	</div>
 	<!---->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-57994233-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </body>
 
 </html>
